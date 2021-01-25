a2b2546d0ba9a0db8f88cba35934ed9c9864ef3d
島村抱月 <shimamura.hougetsu@protonmail.com>
2021-01-24 21:23:56 -0600

    minor edits, published on pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="shinya",
     version="0.1a1",
+    description='A Python package to edit BDMV components like MPLS, CLPI, etc.',
+    url='http://github.com/shimamura-hougetsu/shinya',
+    author='Shimamura Hougetsu',
+    license='MIT',
     packages=find_packages(),
-    install_requires=['lxml'],
+    install_requires=['lxml>=4.6'],
     python_requires=">=3.6"
 )
